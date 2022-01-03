69a411d3f6b03ee6d722043a912b4f27245509b1
ujjual <ujjual72@gmail.com>
2022-01-03 20:50:02 +0530

    error hell

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,20 @@ require('dotenv').config()
 
 const { PROJECTID, PRIVATE_KEY } = process.env;
 
+
+
 module.exports = {
   networks:{
-    hardhat:{
-      chainId: 1337,
-    },
+    // hardhat:{
+    //   chainId: 1337,
+    // },
+    mumbai: {
+      url:`https://polygon-mumbai.g.alchemy.com/v2/${PROJECTID}`,
+      accounts=[`0x${PRIVATE_KEY}`]
+       }
     // mumbai:{
     //   url:`https://polygon-mumbai.g.alchemy.com/v2/${PROJECTID}`,
-    //   accounts=['`0x${PRIVATE_KEY}`']
+    //   accounts=[`0x${PRIVATE_KEY}`]
     // },
     // mainnet:{
     //   url:`https://polygon-mainnet.g.alchemy.com/v2/${PROJECTID}`,
